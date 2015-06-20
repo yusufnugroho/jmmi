3a79adbb8356a1db0b241f30d490d299f912e6a3
yusuf <yusuf_nugroho>
2015-06-20 08:30:30 +0700

    remove /index.php/controller

--- application/config/config.php
+++ application/config/config.php
@@ -16,10 +16,12 @@
 | environments.
 |
 */
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
-
+//Remove /index.php
+/*  $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+    $config['base_url'] .= "://".$_SERVER['HTTP_HOST'];
+    $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']),"",$_SERVER['SCRIPT_NAME']);
+*/
+$config['base_url'] = '';
 /*
 |--------------------------------------------------------------------------
 | Index File
