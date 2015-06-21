9b94ca8cc7660de0b36f18a4e90e3425a34062c2
manggala <dmazter.mgm@gmail.com>
2015-06-21 21:23:59 +0700

    FrontEnd -> Home -> Register/Login Form

---
+++ application/views/frontend/footer/footer.php
@@ -0,0 +1,54 @@
+<?php
+$assets_location = base_url()."assets/basica/";
+?>
+
+	    <!-- Footer -->
+	    <div class="footer">
+	    	<div class="container">
+			
+		    	<div class="row">
+				
+		    		<div class="col-footer col-md-4 col-xs-6">
+		    			<h3>Contact Us</h3>
+		    			<p class="contact-us-details">
+	        				<b>Address:</b> 123 Downtown Avenue, Manhattan, New York, United States of America<br/>
+	        				<b>Phone:</b> +1 123 45678910<br/>
+	        				<b>Fax:</b> +1 123 45678910<br/>
+	        				<b>Email:</b> <a href="mailto:info@yourcompanydomain.com">info@yourcompanydomain.com</a>
+	        			</p>
+		    		</div>				
+		    		<div class="col-footer col-md-4 col-xs-6">
+		    			<h3>Our Social Networks</h3>
+						<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam.</p>
+		    			<div>
+		    				<img src="<?php echo $assets_location;?>img/icons/facebook.png" width="32" alt="Facebook">
+		    				<img src="<?php echo $assets_location;?>img/icons/twitter.png" width="32" alt="Twitter">
+		    				<img src="<?php echo $assets_location;?>img/icons/linkedin.png" width="32" alt="LinkedIn">
+							<img src="<?php echo $assets_location;?>img/icons/rss.png" width="32" alt="RSS Feed">
+						</div>
+		    		</div>
+		    		<div class="col-footer col-md-4 col-xs-6">
+		    			<h3>About Our Company</h3>
+		    				<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat. Ut wisi enim ad minim veniam, quis nostrud exerci.</p>
+		    		</div>
+
+		    	</div>
+		    	<div class="row">
+		    		<div class="col-md-12">
+		    			<div class="footer-copyright">&copy; 2014 <a href="http://www.vactualart.com/portfolio-item/basica-a-free-html5-template/">Basica</a> Bootstrap HTML Template. By <a href="http://www.vactualart.com">Vactual Art</a>.</div>
+		    		</div>
+		    	</div>
+		    </div>
+	    </div>
+
+        <!-- Javascripts -->
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="<?php echo $assets_location;?>js/jquery-1.9.1.min.js"><\/script>')</script>
+        <script src="<?php echo $assets_location;?>js/bootstrap.min.js"></script>
+		
+		<!-- Scrolling Nav JavaScript -->
+		<script src="<?php echo $assets_location;?>js/jquery.easing.min.js"></script>
+		<script src="<?php echo $assets_location;?>js/scrolling-nav.js"></script>		
+
+    </body>
+</html>
